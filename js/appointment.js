1d749c019a734cefb0656bd205d6c1c36a09d717
Jedthongin <jedthongin@gmail.com>
2025-10-04 22:04:38 +0700

    Update appointment.js

--- js/appointment.js
+++ js/appointment.js
@@ -1,20 +1,19 @@
-// js/appointment.js
 import { daysResultDiv } from './dom.js';
 
-export function getAppointmentInfo(appointmentToggle, startDateInput, endDateInput) {
+export function getAppointmentInfo(appointmentToggle, startDateInput, endDateInput){
   let daysUntilAppointment = 7;
   let isAppointmentCalculation = false;
   let startDate = null;
 
-  if (appointmentToggle.checked) {
+  if (appointmentToggle.checked){
     const sv = startDateInput.value;
     const ev = endDateInput.value;
-    if (sv && ev) {
+    if (sv && ev){
       const sd = new Date(sv);
       const ed = new Date(ev);
       sd.setHours(0,0,0,0);
       ed.setHours(0,0,0,0);
-      if (ed > sd) {
+      if (ed > sd){
         const diff = ed.getTime() - sd.getTime();
         daysUntilAppointment = Math.round(diff / (1000*3600*24));
         isAppointmentCalculation = true;
@@ -25,7 +24,7 @@ export function getAppointmentInfo(appointmentToggle, startDateInput, endDateInp
   return { daysUntilAppointment, isAppointmentCalculation, startDate };
 }
 
-export function updateAppointmentDaysDisplay(appointmentToggle, startDateInput, endDateInput) {
+export function updateAppointmentDaysDisplay(appointmentToggle, startDateInput, endDateInput){
   const { daysUntilAppointment, isAppointmentCalculation } =
     getAppointmentInfo(appointmentToggle, startDateInput, endDateInput);
 
